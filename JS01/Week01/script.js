c2f8c2262b4366f48214194b5b3ede5b22100ecd
hosseinz <32279010+hosseinz@users.noreply.github.com>
2019-05-19 12:04:08 +0200

    Create script.js

---
+++ JS01/Week01/script.js
@@ -0,0 +1,61 @@
+// nr 1
+console.log("hello ,world"); //english
+console.log("salam, aalam"); //arabic
+console.log("ciao ,mondo"); //italian
+
+// nr2
+console.log("I'm awesome");
+
+// nr3
+let x ;
+console.log("the value of x will be:", x );
+console.log(x);
+x = 5 ;
+console.log("the value of x will be:", x );
+console.log(x);
+
+// nr4
+
+let y= "salam";
+console.log("The value of y will be: " + y);
+console.log(y);
+
+y= "salam";
+console.log("The value of y will be: " + y);
+console.log(y);
+
+// nr5
+let z = math.round ;
+z = 1.6 ;
+console.log(z); //
+
+// nr6
+let animals =[];
+console.log("empty") ;
+console.log(animal) ;
+
+let favorit_animals= ['dog', 'lion', 'cat', 'birds','fish'];
+console.log(favorit_animals);
+
+favorit_animals.push['zibra'];
+console.log(favorit_animals);
+
+// nr7
+let fruits = ["Banana", "Orange", "Apple", "Mango"];
+let first = fruits[fruits.length-1 ];
+console.log(first);
+
+// nr8 // It was Difficult . I could not solve it
+let foo = 3;
+console.log("The value of my variable foo is: " + foo); 
+
+// nr9
+let d = 3 ;
+d = %3 ;
+console.log(d)
+
+// nr10 // It was Difficult . I could not solve it
+let infinity= k ;
+
+console.log(6/0);
+console.log(6/0 === 10/0);
